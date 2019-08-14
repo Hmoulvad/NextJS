193683f9131aef393b0e995d8c4efba951ed3dc8
Hannibal B. Moulvad <hmoulvad@hotmail.com>
2019-08-14 17:53:40 -0600

    Initial Commit

---
+++ pages/about.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const About: React.FC<any> = () => {
+    return (
+        <div>
+            <p>This is About</p>
+        </div>
+    )
+};
+
+export default About;
---
+++ pages/index.tsx
@@ -0,0 +1,11 @@
+import * as React from "react";
+
+const Index: React.FC<any> = () => {
+    return (
+        <div>
+            <p>This is Index</p>
+        </div>
+    )
+};
+
+export default Index;
